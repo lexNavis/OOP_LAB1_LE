ea6692a028785a0190d24b16c0046020c6e00d06
Shein Denis <ja1lbr3ak@yandex.ru>
2025-03-24 19:51:38 +0300

    added Fish.Show() test

--- OOP_LAB1_LE.cpp
+++ OOP_LAB1_LE.cpp
@@ -1,5 +1,25 @@
+#include <Windows.h>
 #include <iostream>
+#include "IDK.h"
+
+#define KEY_DOWN(vk) (GetAsyncKeyState(vk) & 0x8000)
+
+using namespace std;
+
+void ClearScreen() {
+	HWND hWnd = FindWindowA("ConsoleWindowClass", NULL);
+	HDC hdc = GetDC(hWnd);
+	HBRUSH hBrush = CreateSolidBrush(RGB(255, 255, 255));
+	SelectObject(hdc,hBrush);
+	Rectangle(hdc, 0, 0, 1920, 1080);
+	DeleteObject(hBrush);
+}
 
 int main() {
-	std::cout << "HW!";
+	ClearScreen();
+	int x, y;
+	cout << "Enter (x, y):\n";
+	cin >> x >> y;
+	Fish AFish(x, y);
+	AFish.Show();
 }
