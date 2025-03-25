ba5e8d8b8d29836a2bd7c878e47c6c4a01e52414
Shein Denis <ja1lbr3ak@yandex.ru>
2025-03-25 18:28:04 +0300

    added react and hasCollisionWith

--- IDK.h
+++ IDK.h
@@ -13,61 +13,8 @@ public:
 	void setY(int new_y);
 };
 
-class Fish : public Location {
-public:
-	Fish(int new_x, int new_y);
-	~Fish();
-	void body(); 
-	void rear_fin();
-	void bottom_fin();
-	void top_fin();
-	void eye();
-	void mouth();
-	virtual void Show();
-	virtual void Hide();
-	void moveTo(int new_x, int new_y);
-	void drag(int step);
-};
 
-class HatFish : public Fish {
-public:
-	HatFish(int new_x, int new_y);
-	~HatFish();
-	void hat();
-	virtual void Show();
-	virtual void Hide();
-	// Добавить поведение при столкновении
-	// Например, поднимать шляпу при столкновении с флагом 
-	// и менять шляпу на корону при столкновении с денежкой
-};
-
-class MutantFish : public Fish {
-public:
-	MutantFish(int new_x, int new_y);
-	~MutantFish();
-	void second_eye();
-	virtual void Show();
-	virtual void Hide();
-	// Добавить поведение при столкновении 
-	// Например, убрать хвост при столкновении с флагом 
-	// и добавить плавник при столкновении с денежкой
-};
-
-class CircleFish : public Fish {
-public:
-	CircleFish(int new_x, int new_y);
-	~CircleFish();
-	void body();
-	void bottom_fin();
-	void top_fin();
-	virtual void Show();
-	virtual void Hide();
-	// Добавить поведение при столкновении
-	// Например, сдуться в эллипс при столкновении с флагом 
-	// и стать вертикальным эллипсом при столкновении с денежкой
-};
-
-//abstract obstacle class
+//abstract obstacle class (ith rectangular border)
 class Obstacle : public Location {
 protected:
 	int size_x; // len
@@ -82,4 +29,79 @@ public:
 	void setsizeY(int new_szY);
 	virtual void Show() = 0;
 	virtual void Hide() = 0;
+};
+
+class Flag : public Obstacle {
+protected:
+public:
+	Flag(int new_x, int new_y, int new_szX, int new_szY);
+	~Flag();
+	virtual void Show();
+	virtual void Hide();
+};
+
+class Fish : public Location {
+public:
+	Fish(int new_x, int new_y);
+	~Fish();
+	void body(); 
+	void rear_fin();
+	void bottom_fin();
+	void top_fin();
+	void eye();
+	void mouth();
+	virtual void Show();
+	virtual void Hide();
+	virtual bool hasCollisionWith(Obstacle* obstacle);
+	virtual void react(Flag* flag) = 0;
+	void moveTo(int new_x, int new_y);
+	void drag(int step);
+};
+
+class HatFish : public Fish {
+public:
+	HatFish(int new_x, int new_y);
+	~HatFish();
+	void hat();
+	void hat_up();
+	virtual void Show();
+	virtual void Hide();
+	virtual bool hasCollisionWith(Obstacle* obstacle);
+	virtual void react(Flag* flag);
+	// Добавить поведение при столкновении
+	// Например, поднимать шляпу при столкновении с флагом 
+	// и менять шляпу на корону при столкновении с денежкой
+};
+
+class MutantFish : public Fish {
+public:
+	MutantFish(int new_x, int new_y);
+	~MutantFish();
+	void second_eye();
+	void third_eye();
+	virtual void Show();
+	virtual void Hide();
+	virtual bool hasCollisionWith(Obstacle* obstacle);
+	virtual void react(Flag* flag);
+	// Добавить поведение при столкновении 
+	// Например, добавить третий глаз при столкновении с флагом 
+	// и добавить плавник при столкновении с денежкой
+};
+
+class CircleFish : public Fish {
+public:
+	CircleFish(int new_x, int new_y);
+	~CircleFish();
+	void body();
+	void slim_body();
+	void fat_body();
+	void bottom_fin();
+	void top_fin();
+	virtual void Show();
+	virtual void Hide();
+	virtual bool hasCollisionWith(Obstacle* obstacle);
+	virtual void react(Flag* flag);
+	// Добавить поведение при столкновении
+	// Например, сдуться в эллипс при столкновении с флагом 
+	// и стать вертикальным эллипсом при столкновении с денежкой
 };
