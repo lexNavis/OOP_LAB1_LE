6a8fa026013b8970ee5473c515021b1187a0bdf3
Shein Denis <ja1lbr3ak@yandex.ru>
2025-04-12 14:38:14 +0300

    added menu and OperateWith func

--- OOP_LAB1_LE.cpp
+++ OOP_LAB1_LE.cpp
@@ -6,6 +6,9 @@
 
 using namespace std;
 
+int PPM = 40; //Pixels Per Move
+bool EXIT_TASK = false; //to break infinite loops
+
 void ClearScreen() {
 	HWND hWnd = FindWindowA("ConsoleWindowClass", NULL);
 	HDC hdc = GetDC(hWnd);
@@ -15,15 +18,166 @@ void ClearScreen() {
 	DeleteObject(hBrush);
 }
 
+void OperateWith(Fish* fish) {
+    while (!EXIT_TASK) {
+        cout << "Выберите действие над базовой рыбой: \n"
+            << "1 - Показать на экране\n"
+            << "2 - Скрыть\n"
+            << "3 - Перемещать\n"
+            << "Другая цифра - Вернуться на главную\n";
+        int choice;
+        cin >> choice;
+        system("cls");
+        switch (choice) {
+            case 1: { fish->Show();    break; }
+            case 2: { fish->Hide();    break; }
+            case 3: { fish->drag(PPM); break; }
+            default: {
+                EXIT_TASK = true;
+                cout << "Возращаем...\n";
+                break;
+            }
+        }
+    }
+}
+
 int main() {
-	ClearScreen();
-	int x, y;
-	cout << "Enter (x, y):\n";
-	cin >> x >> y;
-	HatFish AFish(x, y);
-	AFish.Show();
-	system("pause");
-	//AFish.moveTo(x + 40, y);
-	//AFish.Hide();
-	AFish.drag(40);
+    setlocale(LC_ALL, "RUSSIAN");
+    HWND hWnd = FindWindowA("ConsoleWindowClass", NULL);
+    HDC hdc = GetDC(hWnd);
+    HBRUSH hBrush = CreateSolidBrush(RGB(255, 255, 255));
+    SelectObject(hdc, hBrush);
+    Rectangle(hdc, 0, 0, 1920, 1080);
+    int x = 0, y = 0;
+
+    while (1)
+    {
+        EXIT_TASK = false;
+        cout << "Выберите действие: \n"
+            << "1 - Создать базовую рыбу\n"
+            << "2 - Создать рыбу с шляпой\n"
+            << "3 - Создать рыбу с двумя глазами \n"
+            << "4 - Создать рыбу - шар \n"
+            << "5 - Очистить экран \n";
+        int choice;
+        cin >> choice;
+        switch (choice) {
+        case 1: {
+            cout << "Введите координаты (x,y): \n";
+            cin >> x >> y;
+            Fish AFish(x, y);
+            OperateWith(&AFish);
+            /*while (!EXIT_TASK) {
+                cout << "Выберите действие над базовой рыбой: \n"
+                    << "1 - Показать на экране\n"
+                    << "2 - Скрыть\n"
+                    << "3 - Перемещать\n"
+                    << "Другая цифра - Вернуться на главную\n";
+                cin >> choice;
+                system("cls");
+                switch (choice) {
+                case 1: { AFish.Show();    break; }
+                case 2: { AFish.Hide();    break; }
+                case 3: { AFish.drag(PPM); break; }
+                default: {
+                    EXIT_TASK = true;
+                    cout << "Возращаем...\n";
+                    break;
+                }
+                }
+            }*/
+            break;
+        }
+
+        case 2: {
+            cout << "Введите координаты (x,y): \n";
+            cin >> x >> y;
+            HatFish BFish(x, y);
+            OperateWith(&BFish);
+            /*while (!EXIT_TASK) {
+                cout << "Выберите действие над рыбой - патриотом: \n"
+                    << "1 - Показать на экране\n"
+                    << "2 - Скрыть\n"
+                    << "3 - Перемещать\n"
+                    << "4 - Патриотичная форма\n"
+                    << "5 - Вернуться в обычную форму\n"
+                    << "Другая цифра - Вернуться на главную\n";
+                cin >> choice;
+                system("cls");
+                switch (choice) {
+                case 1: { BFish.Show();    break; }
+                case 2: { BFish.Hide();    break; }
+                case 3: { BFish.drag(PPM); break; }
+                default: {
+                    EXIT_TASK = true;
+                    cout << "Возращаем...\n";
+                    break;
+                }
+                }
+            }*/
+            break;
+        }
+        case 3: {
+            cout << "Введите координаты (x,y): \n";
+            cin >> x >> y;
+            MutantFish CFish(x, y);
+            OperateWith(&CFish);
+            /*while (!EXIT_TASK) {
+                cout << "Выберите действие над диско-рыбой: \n"
+                    << "1 - Показать на экране\n"
+                    << "2 - Скрыть\n"
+                    << "3 - Перемещать\n"
+                    << "4 - Диско - форма\n"
+                    << "Другая цифра - Вернуться на главную\n";
+                cin >> choice;
+                system("cls");
+                switch (choice) {
+                case 1: { CFish.Show();    break; }
+                case 2: { CFish.Hide();    break; }
+                case 3: { CFish.drag(PPM); break; }
+                default: {
+                    EXIT_TASK = true;
+                    cout << "Возращаем...\n";
+                    break;
+                }
+                }
+            }*/
+            break;
+        }
+        case 4: {
+            cout << "Введите координаты (x,y): \n";
+            cin >> x >> y;
+            CircleFish DFish(x, y);
+            OperateWith(&DFish);
+            /*while (!EXIT_TASK) {
+                cout << "Выберите действие над диско-рыбой: \n"
+                    << "1 - Показать на экране\n"
+                    << "2 - Скрыть\n"
+                    << "3 - Перемещать\n"
+                    << "4 - Диско - форма\n"
+                    << "Другая цифра - Вернуться на главную\n";
+                cin >> choice;
+                system("cls");
+                switch (choice) {
+                case 1: { DFish.Show();    break; }
+                case 2: { DFish.Hide();    break; }
+                case 3: { DFish.drag(PPM); break; }
+                default: {
+                    EXIT_TASK = true;
+                    cout << "Возращаем...\n";
+                    break;
+                }
+                }
+            }*/
+            break;
+        }
+        case 5: {
+            ClearScreen();
+            break;
+        }
+        default:
+            break;
+        }
+    }
+    return 0;
 }
