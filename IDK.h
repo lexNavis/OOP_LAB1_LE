8963702c89942cd3173fb3a9159f4938576bd372
Shein Denis <ja1lbr3ak@yandex.ru>
2025-05-11 14:40:19 +0300

    added ABC_Object, ABC_Obstacle, IFish

--- IDK.h
+++ IDK.h
@@ -31,26 +31,57 @@ public:
 	void setY(int new_y);
 };
 
-// Класс, описывающий препятствие на экране
-class Obstacle : public Location {
+// Интерфейс для иерархии рыб
+class IFish {
+public:
+	// Отрисовка тела рыбы
+	virtual void body(int clr1, int clr2, int clr3) = 0;
+	// Отрисовка заднего плавника рыбы
+	virtual void rear_fin(int clr1, int clr2, int clr3) = 0;
+	// Отрисовка нижнего плавника рыбы
+	virtual void bottom_fin(int clr1, int clr2, int clr3) = 0;
+	// Отрисовка верхнего плавника рыбы
+	virtual void top_fin(int clr1, int clr2, int clr3) = 0;
+	// Отрисовка глаза рыбы
+	virtual void eye(int clr1, int clr2, int clr3) = 0;
+	// Отрисовка рта рыбы
+	virtual void mouth(int clr1, int clr2, int clr3) = 0;
+};
+
+
+class ABC_Object : public Location {
+protected:
+	int id;
+public:
+	ABC_Object(int new_x, int new_y);
+	~ABC_Object();
+	int getID();
+	virtual void Show() = 0;
+	virtual void Hide() = 0;
+};
+
+// Абстрактный класс, описывающий препятствие на экране
+class ABC_Obstacle : public ABC_Object {
 protected:
 	// Препятствия будут вписаны в прямоугольник с длиной size_x и шириной size_y
 	int size_x;
 	int size_y;
+	// Идентификатор препятствия
 public:
-	Obstacle(int new_x, int new_y, int new_szX, int new_szY);
-	~Obstacle();
+	ABC_Obstacle(int new_x, int new_y, int new_szX, int new_szY);
+	~ABC_Obstacle();
 	int  getsizeX();
 	void setsizeX(int new_szX);
 	int  getsizeY();
 	void setsizeY(int new_szY);
-
-	virtual void Show();
-	virtual void Hide();
+	// Чистая вирутальная функция для отрисовки препятствия
+	virtual void Show() = 0;
+	// Чистая вирутальная функция для сокрытия препятствия
+	virtual void Hide() = 0;
 };
 
 // Класс, описывающий препятствие - флаг
-class Flag : public Obstacle {
+class Flag : public ABC_Obstacle {
 protected:
 public:
 	Flag(int new_x, int new_y, int new_szX, int new_szY);
@@ -61,7 +92,7 @@ public:
 };
 
 // Класс, описывающий препятствие - диск
-class Disc : public Obstacle {
+class Disc : public ABC_Obstacle {
 protected:
 public:
 	Disc(int new_x, int new_y, int new_szX, int new_szY);
@@ -71,18 +102,18 @@ public:
 	virtual void Hide();
 };
 
-class Brick : public Obstacle {
+class Brick : public ABC_Obstacle {
 protected:
 public:
 	Brick(int new_x, int new_y, int new_szX, int new_szY);
 	~Brick();
-	// Переопределеные методы Hide и Show для диска
+	// Переопределеные методы Hide и Show для кирпича
 	virtual void Show();
 	virtual void Hide();
 };
 
 // Класс описывающий точку на экране
-class Point : public Location {
+class Point : public ABC_Object {
 protected:
 	bool visible; // параметр видимости точки на экране
 public:
@@ -91,48 +122,71 @@ public:
 	void setVisible(bool new_visible);
 	bool isVisible();
 
+	// Отображение точки
 	virtual void Show();
+	// Сокрытие точки
 	virtual void Hide();
-
+	// Перемещение точки
 	void moveTo(int new_x, int new_y);
 	int drag(int step);
-
-	virtual bool hasCollisionWith(Obstacle* obstacle);
-
+	// Проверка на столкновение с препятствием
+	virtual bool hasCollisionWith(ABC_Obstacle* obstacle);
 };
 
+
+
+
 // Базовый класс, описывающий рыбу
-class Fish : public Point {
+class Fish : public Point, public IFish {
 protected:
 	// Параметры рыбы
 	int BODY_FOCUS_X;
+	// Ширина тела по У
 	int BODY_FOCUS_Y;
+	// Высота треугольника, задющего задний плавник
 	int REAR_FIN_HEIGHT;
+	// Основание треугольника, задющего задний плавник
 	int REAR_FIN_BASE;
+	// Высота треугольника, задющего верхний плавник
 	int TOP_FIN_HEIGHT;
+	// Основание треугольника, задющего верхний плавник
 	int TOP_FIN_BASE;
+	// Высота треугольника, задющего нижний плавник
 	int BOTTOM_FIN_HEIGHT;
+	// Описание треугольника, задющего нижний плавник
 	int BOTTOM_FIN_BASE;
+	// Радиус глаза
 	int EYE_RADIUS;
+	// Высота треугольника, задающего рот
 	int MOUTH_HEIGHT;
+	// Основание треугольника, задающего рот
 	int MOUTH_BASE;
+	// Идентификатор рыбы
+	
 public:
 	Fish(int new_x, int new_y);
 	~Fish();
-	// Методы, отображающие основные части рыбы на экране
+
+	// Отрисовка тела рыбы
 	void body(int clr1, int clr2, int clr3);
+	// Отрисовка заднего плавника рыбы
 	void rear_fin(int clr1, int clr2, int clr3);
+	// Отрисовка нижнего плавника рыбы
 	void bottom_fin(int clr1, int clr2, int clr3);
+	// Отрисовка верхнего плавника рыбы
 	void top_fin(int clr1, int clr2, int clr3);
+	// Отрисовка глаза рыбы
 	void eye(int clr1, int clr2, int clr3);
+	// Отрисовка рта рыбы
 	void mouth(int clr1, int clr2, int clr3);
 
-	// Переопределеные методы Hide и Show для базовой рыбы
+	// Переопределеные методы Show для базовой рыбы
 	virtual void Show();
+	// Переопределеные методы Hide для базовой рыбы
 	virtual void Hide();
 
 	// Для дальнейшего использования
-	virtual bool hasCollisionWith(Obstacle* obstacle);
+	virtual bool hasCollisionWith(ABC_Obstacle* obstacle);
 	
 };
 
@@ -142,13 +196,15 @@ public:
 	HatFish(int new_x, int new_y);
 	~HatFish();
 
-	// Объявление уникальных методов - шляпы и поднятой шляпы
+	// Объявление уникальных методов - шляпы
 	void hat(int clr1, int clr2, int clr3);
 
+	// Переопределеные методы Show для базовой рыбы
 	virtual void Show();
+	// Переопределеные методы Hide для базовой рыбы
 	virtual void Hide();
-
-	virtual bool hasCollisionWith(Obstacle* obstacle);
+	// Проверка на столкновение с препятствием
+	virtual bool hasCollisionWith(ABC_Obstacle* obstacle);
 	
 };
 
@@ -161,10 +217,13 @@ public:
 	// Объявление уникальных методов - второго глаза
 	void second_eye(int clr1, int clr2, int clr3);
 
+	// Переопределеные методы Show для базовой рыбы
 	virtual void Show();
+	// Переопределеные методы Hide для базовой рыбы
 	virtual void Hide();
 
-	virtual bool hasCollisionWith(Obstacle* obstacle);
+	// Проверка на столкновение с препятствием
+	virtual bool hasCollisionWith(ABC_Obstacle* obstacle);
 
 };
 
@@ -173,23 +232,48 @@ class SquareFish : public Fish {
 public:
 	SquareFish(int new_x, int new_y);
 	~SquareFish();
-	// Объявление уникальных методов -  иных плавников и квадратного тела
+	// Рисование квадратного тела
 	void square_body(int clr1, int clr2, int clr3);
+	// Переопределение рисования нижнего плавника
+	void bottom_fin(int clr1, int clr2, int clr3);
+	// Переопределение рисования верхнего плавника
+	void top_fin(int clr1, int clr2, int clr3);
+	// Переопределеные методы Show для базовой рыбы
+	virtual void Show();
+	// Переопределеные методы Hide для базовой рыбы
+	virtual void Hide();
+	// Проверка на столкновение с препятствием
+	virtual bool hasCollisionWith(ABC_Obstacle* obstacle);
+};
+
+// Производный класс, описывающий рыбу - шар
+class CircleFish : public Fish {
+public:
+	CircleFish(int new_x, int new_y);
+	~CircleFish();
+	// Объявление уникальных методов - круглого, утонченного и утолщенного тел, иного положения плавников
+	void body(int clr1, int clr2, int clr3);
 	void bottom_fin(int clr1, int clr2, int clr3);
 	void top_fin(int clr1, int clr2, int clr3);
 
 	virtual void Show();
 	virtual void Hide();
 
-	virtual bool hasCollisionWith(Obstacle* obstacle);
+	virtual bool hasCollisionWith(ABC_Obstacle* obstacle);
+	
 };
+
+
 
 
 /**********************************************************
 *					ПРОТОТИПЫ ФУНКЦИЙ					  *
 **********************************************************/
-
+// Работа с рыбой через указатель на базовый класс
 void OperateWith(Fish* fish);
+// Демонстрация столкновений
 void demonstrate_collisions();
+// Очистка экрана
 void ClearScreen();
-Fish* setFish(Fish** fishes, int pos);
+// Выбор рыбы из массива
+Fish* setFish(Fish* fishes[5][4], int row, int col);
